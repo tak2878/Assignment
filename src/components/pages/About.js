c7cdc4c7aa6aadf36378c6a677f05dff6b5e256e
Tanveer <t.alam2878@gmail.com>
2020-10-04 16:17:32 +0500

    Assignment

---
+++ src/components/pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const About =()=>{
+      return(
+            <div className="container">
+                  <div className="py-4">
+                        <h1>About Page</h1>
+                        <p className="lead"> Lorem ipsum” dummy text is used by many web-developers to test how their HTML templates will look with real data.
+                        Often, developers use third-party services to generate “Lorem ipsum” text, but now you can do that right in your editor.
+                         Just expand lorem or lipsum abbreviations to get the following snippet </p>
+                        <p className="lead"> Lorem ipsum” dummy text is used by many web-developers to test how their HTML templates will look with real data.
+                        Often, developers use third-party services to generate “Lorem ipsum” text, but now you can do that right in your editor.
+                         Just expand lorem or lipsum abbreviations to get the following snippet </p>
+                        <p className="lead"> Lorem ipsum” dummy text is used by many web-developers to test how their HTML templates will look with real data.
+                        Often, developers use third-party services to generate “Lorem ipsum” text, but now you can do that right in your editor.
+                         Just expand lorem or lipsum abbreviations to get the following snippet </p>
+                  </div>
+            </div>
+      )
+}
+
+export default About;
